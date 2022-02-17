c8441bc5f311ecb68735eb29104746ecfb6fc40d
Abhishek Kothari <abhishek.kothari.1991@gmail.com>
2022-02-17 11:49:34 +0530

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Experius B.V. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Abhishek_Custommodule',
+    __DIR__
+);
